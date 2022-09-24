5248ecb405bc73065a52951a29224818fb46e9e0
Mauro Pereira <mauroapjr@outlook.com>
2022-09-24 13:53:23 -0400

    enable dotenv and remove secrets

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  siteMetadata: {
+    title: `MauroPereira`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-K76XRD91Y5",
+      },
+    },
+    "gatsby-plugin-sitemap",
+  ],
+};
