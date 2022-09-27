af78f99e21e0917218b6167c388dc69a1005a070
Mauro Pereira <mauroapjr@outlook.com>
2022-09-27 18:26:31 -0400

    fix bucket policy

--- terraform/personalsite.tf
+++ terraform/personalsite.tf
@@ -19,6 +19,23 @@ resource "aws_s3_bucket_acl" "b_acl" {
 resource "aws_cloudfront_origin_access_identity" "origin" {
 }
 
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_policy" {
+  bucket = aws_s3_bucket.b.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
