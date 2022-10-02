3d0ae8c746003d86ec42abba59b44b49e5868fd0
Mauro Pereira <mauroapjr@outlook.com>
2022-10-02 18:36:40 -0400

    site deployment

--- terraform/personalsite.tf
+++ terraform/personalsite.tf
@@ -1,6 +1,7 @@
 locals {
   s3_origin_id = "myS3Origin"
   tagName      = "PersonalSite"
+  domain_name  = "mauro-pereira.com"
 }
 
 resource "aws_s3_bucket" "b" {
@@ -51,7 +52,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  #   aliases = ["mysite.example.com", "yoursite.example.com"]
+  aliases = [local.domain_name, "www.${local.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -66,7 +67,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -81,11 +82,81 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate_validation.validation.certificate_arn
+    ssl_support_method  = "sni-only"
   }
 
   tags = {
     Environment = "production"
     Name        = local.tagName
+  }
+}
+
+data "aws_route53_zone" "domain" {
+  zone_id = "Z05289032PUZZ991KNGBH"
+}
+
+resource "aws_acm_certificate" "cert" {
+  provider          = "aws.us-east-1"
+  domain_name       = local.domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${local.domain_name}"
+  ]
+
+  tags = {
+    Name = local.tagName
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "zone_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.domain.zone_id
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  provider                = "aws.us-east-1"
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.zone_record : record.fqdn]
+}
+
+resource "aws_route53_record" "cloudfront" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = local.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloudfront_www" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "www.${local.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = "ca-central-1"
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
