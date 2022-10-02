3b1332bd8b700f6833a709cb231b0d3bd90829ef
Mauro Pereira <mauroapjr@outlook.com>
2022-10-02 18:37:48 -0400

    link idioms with contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,10 +62,10 @@ const IndexPage = ({ data }) => {
       </section>
       {/* =================== About me =================== */}
       <section className="md:flex">
-        <div className="bg-gray-200 pl-4 md:w-1/3 py-10 md:px-40 md:text-right">
+        <div className="bg-gray-200 pl-10 md:w-1/3 py-10 md:px-40 md:text-right">
           <h3 className="text-2xl font-semibold text-blue-900">ABOUT ME</h3>
         </div>
-        <div className="bg-gray-100 md:w-2/3 px-20 py-10 text-gray-800">
+        <div className="bg-gray-100 px-10 md:w-2/3 md:px-20 py-10 text-gray-800">
           <article className="max-w-xl space-y-4 text-justify">
             <ContentfulRichTech richText={data.contentfulHome.intro} />
           </article>
@@ -73,7 +73,7 @@ const IndexPage = ({ data }) => {
       </section>
       {/* =================== Experience =================== */}
       {data.allContentfulExperience.nodes.map((experience, arrayIndex) => (
-        <section className="flex">
+        <section className="flex  last:border-b-0 border-b-2 border-gray-300">
           <div className="bg-gray-100 w-1/3 py-10 md:px-40 text-right md:text-left ">
             {arrayIndex === 0 && (
               <h3 className="text-2xl text-right font-semibold text-blue-900">
@@ -87,7 +87,7 @@ const IndexPage = ({ data }) => {
           </div>
 
           <div className="bg-gray-200 w-2/3 px-20 py-10 text-gray-800">
-            <article className="max-w-xl text-justify text-gray-800 first:mt-0 mt-10 last:border-b-0 border-b-2 border-gray-300">
+            <article className="max-w-xl text-justify text-gray-800 first:mt-0 mt-10">
               <h4 className="font-bold text-xl">{experience.title}</h4>
               <span className="block">{experience.companyName}</span>
               <span className="space-x-8 lock text-sm text-gray-600">
@@ -135,24 +135,18 @@ const IndexPage = ({ data }) => {
         </section>
       </div>
       {/* =================== Idioms =================== */}
-      <div>
-        <section className="flex">
-          <div className="bg-gray-100 w-1/3 py-10 md:px-40 text-right">
-            <h5 className="text-2xl font-semibold text-blue-900">IDIOMS</h5>
-          </div>
-          <div className="bg-gray-200 block w-2/3 px-20 py-10 text-gray-800">
-            <article className="max-w-xl space-y-4 text-justify">
-              Portuguese - Native
-            </article>
-            <article className="max-w-xl space-y-4 text-justify">
-              English - Fluent
-            </article>
-            <article className="max-w-xl space-y-4 text-justify">
-              Spanish - Advanced
-            </article>
-          </div>
-        </section>
-      </div>
+      <section className="flex">
+        <div className="bg-gray-100 w-1/3 py-10 md:px-40 text-right">
+          <h5 className="text-2xl font-semibold text-blue-900">IDIOMS</h5>
+        </div>
+        <div className="bg-gray-200 w-2/3 py-10 px-20 md:px-40 ">
+          {data.allContentfulIdioms.nodes.map((idiom) => (
+            <div className="block ">
+              {idiom.idiom} - {idiom.level}
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
@@ -193,8 +187,14 @@ export const query = graphql`
         }
       }
     }
+    allContentfulIdioms {
+      nodes {
+        idiom
+        level
+      }
+    }
   }
 `;
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = ({ data }) => <title>{data.contentfulHome.title}</title>;
